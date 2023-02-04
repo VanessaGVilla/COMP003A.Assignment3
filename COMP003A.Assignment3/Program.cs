3fda19e92013496cad53e3637992e9f1e5de3c32
Vanessa Garcia <112432406+VanessaGVilla@users.noreply.github.com>
2023-02-03 21:53:32 -0800

    line 5 commit

--- COMP003A.Assignment3/Program.cs
+++ COMP003A.Assignment3/Program.cs
@@ -1,9 +1,31 @@
 ﻿/*Author: Vanessa Garcia
  * Course: Comp - 003A - L01
- * Purpose: Assignment Three 
+ * Purpose: Assignment Three Conditional Statements
 */
 
 
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace ClearConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gradeLetter = "";
+            string numericGradeStr;
+            decimal numericGrade;
+            Console.Write("Enter the number of the grade: ");
+            numericGradeStr = Console.ReadLine();
+            if (decimal.TryParse(numericGradeStr, out numericGrade))
+            {
+                    if (numericGrade > 90)
+                    gradeLetter = "A";
+                    else if (numericGrade >= 80)
+                    gradeLetter = "B";
+                    else if (numericGrade >= 70)
+                    gradeLetter = "C";
+                    else if (numericGrade >= 60)
+                    gradeLetter = "D";
+                    else if (numericGrade < 60)
+                    gradeLetter = "F"
+            }
